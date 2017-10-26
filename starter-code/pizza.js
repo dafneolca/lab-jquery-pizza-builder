e00e8ecb8110e291edbf95122a3eb12504046cf5
Dafne <olca.dafne@gmail.com>
2017-10-26 20:08:54 +0200

    missing final part

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,54 @@
 // Write your Pizza Builder JavaScript in this file.
+function main() {
+  $(".btn-pepperonni").on('click', function() {
+    $(this).toggleClass('active');
+    $(".pep").toggle();
+    $(".pepp").toggle();
+  });
+
+//      $(".element").toggleClass("visible");
+
+
+
+  $(".btn-mushrooms").on('click', function() {
+    $(this).toggleClass('active');
+    $(".mushroom").toggle();
+    $(".shroom").toggle();
+    if ($(".mushroom").toggleClass("visible")) {
+
+    }
+  });
+
+  $(".btn-green-peppers").on('click', function() {
+    $(this).toggleClass('active');
+    $(".green-pepper").toggle();
+    $(".priceGPep").toggle();
+  });
+
+
+  $(".btn-crust").on('click', function() {
+    $(this).toggleClass('active');
+    $('.crust').toggleClass("crust-gluten-free");
+    $(".gfCrust").toggle();
+
+  });
+
+  $(".btn-sauce").on('click', function() {
+    $(this).toggleClass('active');
+    $('.sauce').toggleClass(".sauce-white");
+    $(".sauce-white").toggle();
+    $(".priceWhiteSauce").toggle();
+  });
+}
+
+//         $('.crust').toggleClass("gluten-free-crust");
+
+
+
+
+
+
+
+
+
+$(document).ready(main);
